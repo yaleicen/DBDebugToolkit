d5cb98420c81627fce2ef63bbe079d6ae00401b3
Yalei Cen <116703526+yaleicen@users.noreply.github.com>
2025-01-16 16:36:53 +0800

    Update 1.0.2 (#3)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Example/DBDebugToolkit/DBAppDelegate.m
+++ Example/DBDebugToolkit/DBAppDelegate.m
@@ -53,7 +53,7 @@
 
     [[self.session dataTaskWithURL:url] resume];
     
-    NSLog(@"Test release then merge branches 2");
+    NSLog(@"Test release then merge branches 1.0.2");
 
     return YES;
 }
